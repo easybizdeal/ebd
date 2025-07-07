d7124f16a2ce2d215729198d9ba02b17d737bad1
easybizdeal <freelancer217698@gmail.com>
2025-07-08 01:26:22 +0600

    Fix SQLite path for Render

--- config.py
+++ config.py
@@ -2,9 +2,14 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'easybiz.db')
+    # Store DB in /tmp for Render compatibility
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join('/tmp', 'easybiz.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Hostinger SMTP
